ee70c185ffe12fcd651c17522dd2368590a63777
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2023-02-06 11:24:01 +0100

    UNKNOWN token

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,9 +1,9 @@
 BLANK = "BLANK"
 SILENCE = "SIL"
+UNKNOWN = "UNK"
 
 
 def text_processor(s, vocabulary):
     tokens = list(s)
-    tokens_numerized = [vocabulary[token] for token in tokens]
-
+    tokens_numerized = [vocabulary.get(token, vocabulary[UNKNOWN]) for token in tokens]
     return tokens_numerized
